7806d1e8ae417463780375a38b422ec8bc56a141
Michele <m.buccoli@gmail.com>
2016-08-06 17:50:31 -0400

    going toward unification

--- server-side/Judge.py
+++ server-side/Judge.py
@@ -1,5 +1,14 @@
+import sys
+sys.path.append('../audio2tokens')
+from audio2tokens import *
 download_dir=''
+
+def retrieve_tokens(audiofile):
+	audio_path_raw = _raw_audio(audiofile)
+    tokens = _get_text(audio_path_raw)
+	return tokens 
 
 def rank(namefile):
 	audiofile=os.path.join(download_dir,namefile)
+    tokens=retrieve_tokens(audiofile)
 	return {'score':0.1, 'rhymes_scheme':6}
