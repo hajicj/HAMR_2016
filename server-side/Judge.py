5c5e0c7809d8db77327dc259df0547579e515a54
Michele <m.buccoli@gmail.com>
2016-08-15 10:52:46 +0200

    Add some functions to remove wave files

--- server-side/Judge.py
+++ server-side/Judge.py
@@ -4,10 +4,22 @@ from audio2tokens import *
 download_dir='/home/michele/Downloads'
 from rhyme import  get_score
 import time
+
+
+def remove_audiofiles(audio_path):
+    audio_path_16k = audio_path.replace('.wav','_16k.wav')
+    audio_path_raw = audio_path.replace('.wav','.raw')
+
+    os.remove(audio_path)
+	os.remove(audio_path_16k)    
+    os.remove(audio_path_raw)
+
+
 def retrieve_tokens(audiofile):
 	#time.sleep(3)
 	audio_path_raw = raw_audio(audiofile.replace(':','-'))
 	tokens = get_text(audio_path_raw)
+	remove_audiofiles(audio_path_raw)
 	print(tokens)
 	return tokens 
 
