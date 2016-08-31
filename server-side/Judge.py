195d97927bd6356a92b18c142d9695162c1fb565
Michele <m.buccoli@gmail.com>
2016-08-31 16:34:24 +0200

    modifying some small indent

--- server-side/Judge.py
+++ server-side/Judge.py
@@ -11,27 +11,27 @@ def remove_audiofiles(audio_path):
     audio_path_raw = audio_path.replace('.wav','.raw')
 
     os.remove(audio_path)
-	os.remove(audio_path_16k)    
+    os.remove(audio_path_16k)    
     os.remove(audio_path_raw)
 
 
 def retrieve_tokens(audiofile):
-	#time.sleep(3)
-	audio_path_raw = raw_audio(audiofile.replace(':','-'))
-	tokens = get_text(audio_path_raw)
-	remove_audiofiles(audio_path_raw)
-	print(tokens)
-	return tokens 
+    #time.sleep(3)
+    audio_path_raw = raw_audio(audiofile.replace(':','-'))
+    tokens = get_text(audio_path_raw)
+    remove_audiofiles(audio_path_raw)
+    print(tokens)
+    return tokens 
 
 def rank(namefile):
-	#audiofile=os.path.join(download_dir,namefile)
-	tokens=retrieve_tokens(namefile)
+    #audiofile=os.path.join(download_dir,namefile)
+    tokens=retrieve_tokens(namefile)
 
-	if tokens :
-		score = get_score({'text':tokens})
-	else:
-		score = None
-		print "No tokens found"
+    if tokens :
+        score = get_score({'text':tokens})
+    else:
+        score = None
+        print "No tokens found"
 
-	print(score)
-	return {'score':score, 'rhymes_scheme':6}
+    print(score)
+    return {'score':score, 'rhymes_scheme':6}
